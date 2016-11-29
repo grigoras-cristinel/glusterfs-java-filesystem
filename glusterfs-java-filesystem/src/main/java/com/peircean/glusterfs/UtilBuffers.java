5fe55611682db4d3e0f85f1e567047ae0952d820
Grigoras Cristinel <Grigoras.Cristinel@gmail.com>
2016-11-29 16:52:52 +0200

    -- transferTo buffer size question

--- glusterfs-java-filesystem/src/main/java/com/peircean/glusterfs/UtilBuffers.java
+++ glusterfs-java-filesystem/src/main/java/com/peircean/glusterfs/UtilBuffers.java
@@ -2,29 +2,27 @@ package com.peircean.glusterfs;
 
 import java.lang.ref.SoftReference;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 
-@SuppressWarnings("rawtypes")
+@SuppressWarnings({ "rawtypes", "unchecked" })
 public class UtilBuffers {
 
 	private static final int TEMP_BUF_POOL_SIZE = 3;
 	// Per-thread soft cache of the last temporary direct buffer
 
-	private static ThreadLocal[] bufferPool;
+	private static ThreadLocal<SoftReference<ByteBuffer>>[] bufferPool;
 
 	static {
 		bufferPool = new ThreadLocal[TEMP_BUF_POOL_SIZE];
 		for (int i = 0; i < TEMP_BUF_POOL_SIZE; i++)
-			bufferPool[i] = new ThreadLocal();
+			bufferPool[i] = new ThreadLocal<>();
 	}
 
-	@SuppressWarnings("unchecked")
 	static ByteBuffer getTemporaryDirectBuffer(int size) {
 		ByteBuffer buf = null;
 		// Grab a buffer if available
 		for (int i = 0; i < TEMP_BUF_POOL_SIZE; i++) {
-			SoftReference ref = (SoftReference) (bufferPool[i].get());
-			if ((ref != null) && ((buf = (ByteBuffer) ref.get()) != null) && (buf.capacity() >= size)) {
+			SoftReference<ByteBuffer> ref = bufferPool[i].get();
+			if ((ref != null) && ((buf = ref.get()) != null) && (buf.capacity() >= size)) {
 				buf.rewind();
 				buf.limit(size);
 				bufferPool[i].set(null);
@@ -33,7 +31,7 @@ public class UtilBuffers {
 		}
 
 		// Make a new one
-		return ByteBuffer.allocateDirect(size);
+		return ByteBuffer.allocate(size);
 	}
 
 	static void releaseTemporaryDirectBuffer(ByteBuffer buf) {
@@ -41,25 +39,28 @@ public class UtilBuffers {
 			return;
 		// Put it in an empty slot if such exists
 		for (int i = 0; i < TEMP_BUF_POOL_SIZE; i++) {
-			SoftReference ref = (SoftReference) (bufferPool[i].get());
+			SoftReference<ByteBuffer> ref = (bufferPool[i].get());
 			if ((ref == null) || (ref.get() == null)) {
-				bufferPool[i].set(new SoftReference(buf));
+				bufferPool[i].set(new SoftReference<>(buf));
 				return;
 			}
 		}
 		// Otherwise replace a smaller one in the cache if such exists
 		for (int i = 0; i < TEMP_BUF_POOL_SIZE; i++) {
-			SoftReference ref = (SoftReference) (bufferPool[i].get());
+			SoftReference ref = (bufferPool[i].get());
 			ByteBuffer inCacheBuf = (ByteBuffer) ref.get();
 			if ((inCacheBuf == null) || (buf.capacity() > inCacheBuf.capacity())) {
-				bufferPool[i].set(new SoftReference(buf));
+				bufferPool[i].set(new SoftReference<>(buf));
 				return;
 			}
 		}
 	}
 
 	static void erase(ByteBuffer bb) {
-		Arrays.fill(bb.array(), (byte) 0);
+		byte[] inar = bb.array();
+		for (int i = 0; i < inar.length; i++) {
+			inar[i] = 0;
+		}
 	}
 
 }
